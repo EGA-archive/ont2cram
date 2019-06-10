258e2515c700b13f40fee94784d37e195f56e2ff
alexanderv@ebi.ac.uk <alexanderv@ebi.ac.uk>
2019-06-10 17:13:42 +0100

    improved tests

--- test.py
+++ test.py
@@ -12,16 +12,7 @@ def h5dump_all_files_in_dir(input_dir, output_dir):
     with open(dump_path,'w') as outfile:
         subprocess.call(["h5dump",file_path], stdout=outfile)
 
-def assert2FilesEqual(path1, path2):
-    with open(path1) as f1:
-        with open(path2) as f2:    
-            assert( [row for row in f1] == [row for row in f2] )
     
-def assert2DirsEqual(dir1, dir2):
-    dir1_files = os.listdir(dir1)
-    dir2_files = os.listdir(dir2)
-    assert( len(dir1)==len(dir2) )
-    for f in dir1_files: assert2FilesEqual( os.path.join(dir1,f), os.path.join(dir2,f) )      
 
 class Ont2CramTests(unittest.TestCase):
     def setUp(self):
@@ -35,6 +26,17 @@ class Ont2CramTests(unittest.TestCase):
         shutil.rmtree(self.h5dump_original_dir)
         shutil.rmtree(self.h5dump_restored_dir)                                                                
 
+    def assert2FilesEqual(self, path1, path2):
+        with open(path1) as f1:
+            with open(path2) as f2:
+                self.assertEquals( [row for row in f1] ,  [row for row in f2] )
+                   
+    def assert2DirsEqual(self, dir1, dir2):
+        dir1_files = os.listdir(dir1)
+        dir2_files = os.listdir(dir2)
+        self.assertTrue( len(dir1)==len(dir2) )
+        for f in dir1_files: self.assert2FilesEqual( os.path.join(dir1,f), os.path.join(dir2,f) )                 
+
     def test_round_trip(self):
         cram_path = tempfile.mkstemp()[1]        
         try:
@@ -46,6 +48,6 @@ class Ont2CramTests(unittest.TestCase):
             h5dump_all_files_in_dir( self.fast5_origial_dir,  self.h5dump_original_dir )    
             h5dump_all_files_in_dir( self.fast5_restored_dir, self.h5dump_restored_dir )   
 
-            assert2DirsEqual( self.h5dump_original_dir, self.h5dump_restored_dir )            
+            self.assert2DirsEqual( self.h5dump_original_dir, self.h5dump_restored_dir )            
         finally:
-            os.remove(cram_path)           
+            os.remove(cram_path)
