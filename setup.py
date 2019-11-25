f85c9240b1d7f629de0df7b2311547375b7a1e80
a-slide <aleg@ebi.ac.uk>
2019-11-25 20:37:37 +0000

    Update setup for pip

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
-import ont2cram as package
+import ont2cram as pkg
 
 with open("README.md") as f:
     long_description = f.read()
 
 setup(
-    name=package.__name__,
-    description=package.__description__,
+    name=pkg.__name__,
+    description=pkg.__description__,
     long_description = long_description,
     long_description_content_type="text/markdown",
-    version=package.__version__,
-    author=package.__author__,
+    version=pkg.__version__,
+    author=pkg.__author__,
     install_requires=[
         'h5py>=2.10',
         "ont_fast5_api>=2.0.0",
@@ -18,16 +18,19 @@ setup(
         "pysam>=0.15.3",
         "numpy>=1.17.4 ",
         "parameterized>=0.7.1"],
-    keywords=package.__keywords__,
-    license=package.__license__,
-    url=package.__url__,
+    keywords=pkg.__keywords__,
+    license=pkg.__license__,
+    url=pkg.__url__,
+    python_requires ='>=3.5',
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3"
     ],
-    package_dir = {package.__name__: package.__name__},
-    package_data = {package.__name__: ['test/test_data/*', 'test/test_data/*/*']},
+    pkgs = [pkg.__name__],
+    pkg_dir = {pkg.__name__: pkg.__name__},
+    pkg_dir = {pkg.__name__: pkg.__name__},
+    pkg_data = {pkg.__name__: ['test/test_data/*', 'test/test_data/*/*']},
     entry_points = {'console_scripts': ['ont2cram=ont2cram.__main__:main']}
 )
